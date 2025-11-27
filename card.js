1fae5a99213dabe67a284fbfaa93a29f1024c7d8
Amangeldi96 <193253467+Amangeldi96@users.noreply.github.com>
2025-11-28 02:05:23 +0600

    Обновил card.js

--- card.js
+++ card.js
@@ -83,10 +83,6 @@ document.addEventListener("DOMContentLoaded", () => {
     alert("Заполните все поля и добавьте хотя бы одно фото.");
     return;
   }
-	   let phone = document.getElementById("phone").value.trim();
-      const category = categorySelect.value;
-      const descText = desc.value.trim();
-      const price = document.getElementById("price").value.trim();
 
       phone = phone.replace(/\D/g, "");
       if (phone.startsWith("996")) {
